6e335b9ca532b2aba670f69efd1427317dbfe5ee
vaishnavwalunj <56834961+vaishnavwalunj@users.noreply.github.com>
2019-10-21 20:15:01 +0530

    added modal for updates

--- footbar.php
+++ footbar.php
@@ -17,12 +17,12 @@
             <div  class=" footbar col-3 my-auto">
                 <a href="search.php"><i class="icon fa">&#xf002;</i></a>
             </div>
-            <div  class=" footbar col-3 my-auto" >
-                <a href="upload.php"><i class="icon fas fa-upload" ></i></a>
-            </div>
-            <div  class=" footbar col-3 my-auto">
-                <a href="profile.php"><i class="icon fas fa-user-alt"></i></a>
-            </div>
+            <div class="alert alert-success" role="alert">
+  <h4 class="alert-heading">Well done!</h4>
+  <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+  <hr>
+  <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+</div>
         </div>
     </div>
     <!-- <script src="script.js"></script> -->
